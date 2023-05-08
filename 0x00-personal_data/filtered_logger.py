d9a908c3e7d330036422e096120368638d8c6083
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-05-08 22:21:12 +0300

    add logger and db functions

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,31 @@
     filtered_logger.py
 """
 import re
-from typing import List
+import os
+import mysql.connector
+from typing import List, Tuple
+import logging
+PII_FIELDS: Tuple[str] = ('email', 'phone', 'ssn', 'password', 'ip')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializer function"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """output message in logging format"""
+        # NotImplementedError
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -19,3 +43,24 @@ def replace(f: List[str], sep: str, red: str, m: str) -> str:
     pattern = f + '=.*?' + sep
     repl = f + '=' + red + sep
     return re.sub(pattern, repl, m)
+
+
+def get_logger() -> logging.Logger:
+    """return a logger object"""
+    logger = logging.getLogger('user_data')
+    logger.propagate = False
+    logger.setLevel('INFO')
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter())
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector:
+    """return a database connector"""
+    db = os.getenv('PERSONAL_DATA_DB_NAME')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    pwd = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    return mysql.connector.connect(user=user, password=pwd, host=host,
+                                   database=db)
