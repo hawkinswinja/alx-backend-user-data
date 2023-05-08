7dc2b674ec732930bb15d8bfcd3b13f5064f745c
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-05-08 19:21:39 +0300

    add function notations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,17 +3,19 @@
     filtered_logger.py
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """filter and obfuscate user PII"""
     for field in fields:
         message = replace(field, separator, redaction, message)
     return message
 
 
-def replace(f, sep, red, message):
+def replace(f: List[str], sep: str, red: str, m: str) -> str:
     """substitutes the matched pattern with the reduction"""
     pattern = f + '=.*?' + sep
     repl = f + '=' + red + sep
-    return re.sub(pattern, repl, message)
+    return re.sub(pattern, repl, m)
